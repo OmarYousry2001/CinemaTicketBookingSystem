2bbaf19d410cd4c9cfcf9baf9d6f3f897a631494
omar <OmarYoussry2001>
2025-07-14 01:20:33 +0300

    1- Create Generic Service 2-Create  Acutor Service

---
+++ CinemaTicketBookingSystem.Service/Abstracts/IActorService.cs
@@ -0,0 +1,18 @@
+﻿using CinemaTicketBookingSystem.Data.Entities;
+using CinemaTicketBookingSystem.Service.ServiceBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaTicketBookingSystem.Service.Abstracts
+{
+    public interface IActorService : IBaseService<Actor>
+    {
+        Task<bool> IsExistAsync(Guid id);
+        Task<bool> IsExistByNameAsync(string firstName, string lastName);
+        Task<bool> IsExistByNameExcludeItselfAsync(Guid id, string firstName, string lastName);
+        
+    }
+}
---
+++ CinemaTicketBookingSystem.Service/Implementations/ActorService.cs
@@ -0,0 +1,46 @@
+﻿using CinemaTicketBookingSystem.Data.Entities;
+using CinemaTicketBookingSystem.Infrastructure.InfrastructureBases.Repositories;
+using CinemaTicketBookingSystem.Service.Abstracts;
+using CinemaTicketBookingSystem.Service.ServiceBase;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaTicketBookingSystem.Service.Implementations
+{
+    public class ActorService  :BaseService<Actor>  ,IActorService
+    {
+      private readonly ITableRepositoryAsync<Actor> _tableRepositoryAsync;
+    public ActorService(ITableRepositoryAsync<Actor> tableRepositoryAsync) : base(tableRepositoryAsync)
+    {
+            _tableRepositoryAsync = tableRepositoryAsync;
+    }
+
+        public async Task<bool> IsExistByNameExcludeItselfAsync(Guid id, string firstName, string lastName)
+        {
+            return await _tableRepositoryAsync.GetTableNoTracking().AnyAsync(d =>
+                d.Id != id &&
+                d.FirstName.ToLower().Trim() == firstName.ToLower().Trim() &&
+                d.LastName.ToLower().Trim() == lastName.ToLower().Trim());  
+        }
+
+
+        public async Task<bool> IsExistByNameAsync(string firstName, string lastName)
+        {
+            return await _tableRepositoryAsync.GetTableNoTracking()
+                .AnyAsync(d =>
+                    d.FirstName.Trim().ToLower() == firstName.Trim().ToLower() &&
+                    d.LastName.Trim().ToLower() == lastName.Trim().ToLower());
+        }
+
+
+        public async Task<bool> IsExistAsync(Guid id)
+        {
+            return await _tableRepositoryAsync.GetTableNoTracking()
+         .AnyAsync(d => d.Id == id);
+        }
+    }
+}
