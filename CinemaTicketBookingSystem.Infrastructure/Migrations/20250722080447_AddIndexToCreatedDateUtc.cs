6b3b5ce004bbf4d5eeb2c87983c11d7eda65db9b
omar <OmarYoussry2001>
2025-07-22 11:06:12 +0300

    Add Index To Column CreatedDateUtc in Table Resrvation

---
+++ CinemaTicketBookingSystem.Infrastructure/Migrations/20250722080447_AddIndexToCreatedDateUtc.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CinemaTicketBookingSystem.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIndexToCreatedDateUtc : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservations_CreatedDateUtc",
+                table: "Reservations",
+                column: "CreatedDateUtc");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Reservations_CreatedDateUtc",
+                table: "Reservations");
+        }
+    }
+}
